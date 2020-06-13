c8a37c75bc42e8e3b036e780eaadcc3fc7386461
AlexIsOK <alexep2020@gmail.com>
2020-06-12 21:17:01 -0700

    Do not delete old tags

--- main.js
+++ main.js
@@ -64,7 +64,8 @@ function main() {
 
     const path = 'BUILD_NUMBER/BUILD_NUMBER';
     const prefix = env.INPUT_PREFIX ? `${env.INPUT_PREFIX}-` : '';
-
+    const currentVersion = `${env.INPUT_VERSION}`;
+    //${currentVersion}
     //See if we've already generated the build number and are in later steps...
     if (fs.existsSync(path)) {
         let buildNumber = fs.readFileSync(path);
@@ -82,7 +83,7 @@ function main() {
         }
     }
 
-    request('GET', `/repos/${env.GITHUB_REPOSITORY}/git/refs/tags/${prefix}build-number-`, null, (err, status, result) => {
+    request('GET', `/repos/${env.GITHUB_REPOSITORY}/git/refs/tags/${prefix}${currentVersion}-`, null, (err, status, result) => {
     
         let nextBuildNumber, nrTags;
     
@@ -91,13 +92,13 @@ function main() {
             nextBuildNumber = 1;
             nrTags = [];
         } else if (status === 200) {
-            const regexString = `/${prefix}build-number-(\\d+)$`;
+            const regexString = `/${prefix}${currentVersion}-(\\d+)$`;
             const regex = new RegExp(regexString);
             nrTags = result.filter(d => d.ref.match(regex));
             
             const MAX_OLD_NUMBERS = 5; //One or two ref deletes might fail, but if we have lots then there's something wrong!
             if (nrTags.length > MAX_OLD_NUMBERS) {
-                fail(`ERROR: Too many ${prefix}build-number- refs in repository, found ${nrTags.length}, expected only 1. Check your tags!`);
+                fail(`ERROR: Too many ${prefix}${currentVersion}- refs in repository, found ${nrTags.length}, expected only 1. Check your tags!`);
             }
             
             //Existing build numbers:
@@ -117,7 +118,7 @@ function main() {
         }
 
         let newRefData = {
-            ref:`refs/tags/${prefix}build-number-${nextBuildNumber}`, 
+            ref:`refs/tags/${prefix}${currentVersion}-${nextBuildNumber}`, 
             sha: env.GITHUB_SHA
         };
     
@@ -134,21 +135,7 @@ function main() {
             //Save to file so it can be used for next jobs...
             fs.writeFileSync('BUILD_NUMBER', nextBuildNumber.toString());
             
-            //Cleanup
-            if (nrTags) {
-                console.log(`Deleting ${nrTags.length} older build counters...`);
             
-                for (let nrTag of nrTags) {
-                    request('DELETE', `/repos/${env.GITHUB_REPOSITORY}/git/${nrTag.ref}`, null, (err, status, result) => {
-                        if (status !== 204 || err) {
-                            console.warn(`Failed to delete ref ${nrTag.ref}, status: ${status}, err: ${err}, result: ${JSON.stringify(result)}`);
-                        } else {
-                            console.log(`Deleted ${nrTag.ref}`);
-                        }
-                    });
-                }
-            }
-
         });
     });
 }
